4b45c6548a8ddff64f052448fb169e1babaeb30e
Alexandr Sharganov <allinshelter@gmail.com>
2023-08-10 06:28:56 +0400

    create follow model and post_save method to users.models

--- nek/users/admin.py
+++ nek/users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User, Follow
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    """Админка пользователей."""
+
+    list_display = (
+        'id', 'username', 'email', 'first_name',
+        'last_name', 'password'
+    )
+    list_display_links = ('id', 'username')
+    search_fields = ('username', 'email',)
+    list_filter = ('email', 'username')
+    empty_value_display = '-пусто-'
+    
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    """Админка подписок"""
+
+    list_display = (
+        'author', 'user', 
+    )
--- nek/users/models.py
+++ nek/users/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+@receiver(post_save, sender=User)
+def save_or_create_create(sender, instance, created, **kwargs):
+    if created:
+        Blog.objects.create(user=instance)
+    # else:
+    #     try:
+    #         instance.blog.save()
+    #     except ObjectDoesNotExist:
+    #         Blog.objects.create(user=instance)
+
+
+class Follow(models.Model):
+    """Модель подписок на пользователей."""
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Пользователь'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        verbose_name = 'Follow'
+        verbose_name_plural = 'Follows'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_follow'
+            ),
+            models.CheckConstraint(
+                check=~models.Q(user=models.F('author')),
+                name='check_self_following'),
+        ]
+
+    def __str__(self):
+        return f'{self.user} {self.author}'
